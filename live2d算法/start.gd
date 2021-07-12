0ff82a5dd36c1282c050cd132f3596eab03acd1f
2439905184 <suchenglong018@126.com>
2021-07-12 15:14:28 +0800

    优化启动时间 修复导入外部图片的功能
    1.优化启动时间
    2. 修复导入外部图片的功能

--- live2d算法/start.gd
+++ live2d算法/start.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	yield(get_tree().create_timer(3.0),"timeout")
+	yield(get_tree().create_timer(2.0),"timeout")
 	var editor=load("res://live2d算法/头发动起来.tscn")
 	get_tree().change_scene_to(editor)
 	pass # Replace with function body.
