d6731373f525579daea474eeaf8251d44676f20d
2439905184 <suchenglong018@126.com>
2021-07-16 20:29:55 +0800

    确认AnimData类没有问题
    可能是gui编程逻辑顺序出的问题

--- live2d/test/动态添加动画.gd
+++ live2d/test/动态添加动画.gd
@@ -4,4 +4,10 @@ func _ready():
 	var rotation_value=20
 	var key=1
 	var time=2
-
+	#AnimData.test()
+	AnimData.set_loop(true)
+	AnimData.set_reg_key_time(2.0)
+	AnimData.set_time_length(4)
+	AnimData.set_property("rect_rotation")
+	AnimData.reg_key(180,$TextureRect)
+	AnimData.play()
