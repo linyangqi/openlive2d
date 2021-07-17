012e9c3eb9fb5ec604cb04cc43c07aca0d5dd67b
2439905184 <suchenglong018@126.com>
2021-07-17 13:08:00 +0800

    添加关键帧
    但是编辑器不会做 参考godot AnimePlayer插件代码吧
    看源码

--- live2d/scene/Live2dAnim.gd
+++ live2d/scene/Live2dAnim.gd
@@ -1,6 +1,22 @@
 extends Node2D
 onready var animPlayer=$AnimationPlayer
 signal sync_anim_add
+var mode=""
+#同步建模编辑器的数据
+func sync_data_fromModel_editor():
+
+	pass
+func test():
+	
+	pass
+func _process(delta):
+	if mode=="rotate":
+		$Sprite.look_at(get_global_mouse_position())
+func _input(event):
+	if event.is_action_pressed("rotate"):
+		mode="rotate"
+	if event is InputEventMouseButton:
+		mode=""
 #添加动画
 func _on_ask_new_anim_add_anim(animName):
 	var animation=Animation.new()
--- live2d/scene/ask_addTrackNode.gd
+++ live2d/scene/ask_addTrackNode.gd
@@ -5,4 +5,6 @@ func _on_ItemList_item_selected(index):
 	print(index)
 	var select_node_name=$vbox/ItemList.get_item_text(index)
 	print("选中了节点>",select_node_name)
-	
+	if select_node_name=="测试":
+		#添加轨道
+		pass
